1cde3589e2da38886a0401766b503bb6128f7b24
Hugo-W <hugo.weissbart@gmail.com>
2021-03-30 17:49:32 +0200

    modified mandala sketch

--- p5sketch/RadialBeauty/sketch.js
+++ p5sketch/RadialBeauty/sketch.js
@@ -1,13 +1,17 @@
 
 // 
 // Author: Hugo Weissbart
+
+let set;
+
 function setup() {
     initializeFields();
-    createCanvas(600, 400);
+    createCanvas(800, 600);
     background(color(0x11, 0x11, 0x11));
     stroke(color(0x52, 0x70, 0xB9, 50));
     noFill();
     frameRate(500);
+    set = 0;
 }
 
 function draw() {
@@ -21,19 +25,33 @@ function draw() {
     var ampY = 300;
     translate(width / 2, height / 2);
     rotate(t / 200);
-    // Mandala 1
-    // ellipse(cos(t/periodX) * ampX, sin(t/periodY) * ampY,  // position
-    // cos(t/periodX) * ampX, sin(t/periodY) * ampY); // radius
-    // Mandala 2
-    // line(cos(t/periodX) * 300, sin(t/periodY) * 200,  // position A
-    // cos(t/radX) * 300, sin(t/radY) * 200); // position B
-    // Mandala set 3
-    line(// position A
-    sin(t / 60) * 200 + cos(t / 30) * 30, // position A
-    cos(t / 60) * 200 + sin(t / 30) * 30, cos(t / 60) * 200 + sin(t / 30) * 30, // position B
-    cos(t / 60) * 200 + cos(t / 30) * 30);
+
+    if (set == 0) {
+        // Mandala 1
+        ellipse(cos(t/periodX) * ampX, sin(t/periodY) * ampY,  // position
+        cos(t/periodX) * ampX, sin(t/periodY) * ampY); // radius
+    }
+    if (set == 1) {
+        // Mandala 2
+        line(cos(t/periodX) * 300, sin(t/periodY) * 200,  // position A
+        cos(t/radX) * 300, sin(t/radY) * 200); // position B
+    }
+    if (set == 2) {
+        // Mandala set 3
+        line(
+            sin(t / 60) * 200 + cos(t / 30) * 30,
+            cos(t / 60) * 200 + sin(t / 30) * 30,
+            
+            cos(t / 60) * 200 + sin(t / 30) * 30, 
+            cos(t / 60) * 200 + cos(t / 30) * 30
+            );
+    }
 }
 
 function initializeFields() {
 }
 
+function mousePressed() {
+    set += 1;
+    set = set % 3;
+}
